df9ac457d600a44ff400d7dee41e08482380b460
matthewcosgrove <mc.matthew.cosgrove@gmail.com>
2019-01-14 07:00:38 +0000

    For jumpbox ssh script cover cases where VM needed rebuilding since last login

--- envs/aws_default_vpc/jumpbox-ssh.sh
+++ envs/aws_default_vpc/jumpbox-ssh.sh
@@ -14,6 +14,7 @@ SCRIPT_DIR_STATE="$SCRIPT_DIR/${STATE_DIR:=state}"
 echo "Using state stored in $SCRIPT_DIR_STATE"
 export TERRAFORM_STATE="$SCRIPT_DIR_STATE/terraform.tfstate"
 source $SCRIPT_DIR/output-terraform
+ssh-keygen -R ${elastic_ip} # cover cases where VM needed rebuilding since last login
 bosh int $SCRIPT_DIR_STATE/creds.yml --path /jumpbox_ssh/private_key > $TMPDIR/jumpbox.pem && chmod 600 $TMPDIR/jumpbox.pem
 ssh -A jumpbox@${elastic_ip} \
   -i $TMPDIR/jumpbox.pem
